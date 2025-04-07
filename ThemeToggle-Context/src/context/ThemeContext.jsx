a5d3851d0aa6a10ee8f357065685de4824bd9653
Espadv69 <aitorjob69@gmail.com>
2025-04-07 18:44:51 +0200

    add initial state for theme management in ThemeContext

--- ThemeToggle-Context/src/context/ThemeContext.jsx
+++ ThemeToggle-Context/src/context/ThemeContext.jsx
@@ -1,3 +1,11 @@
 import { createContext, useReducer } from 'react'
 import { themeReducer } from './themeReducer'
 import { TOGGLE_THEME } from './themeTypes'
+
+// Create a context for the theme
+export const ThemeContext = createContext()
+
+// Create a initial state for the theme
+const initialState = {
+  theme: 'light',
+}
