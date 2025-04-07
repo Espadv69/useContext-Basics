e9ac5aeb59038b286e108e08ed4b2823ee21ffa9
Espadv69 <aitorjob69@gmail.com>
2025-04-07 19:33:09 +0200

    implement ThemeToggleButton component to toggle between light and dark themes

--- ThemeToggle-Context/src/components/ThemeToggleButton.jsx
+++ ThemeToggle-Context/src/components/ThemeToggleButton.jsx
@@ -1,6 +1,14 @@
 import { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 
-const ThemeToggleButton = () => {}
+const ThemeToggleButton = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+
+  return (
+    <button onClick={toggleTheme} className="toggle-btn">
+      Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+    </button>
+  )
+}
 
 export default ThemeToggleButton
