27e2840185f8ea6653dd9d91ffd865aef084822b
Espadv69 <aitorjob69@gmail.com>
2025-04-07 19:28:37 +0200

    implement ThemedContent component to display content based on the selected theme

--- ThemeToggle-Context/src/components/ThemedContent.jsx
+++ ThemeToggle-Context/src/components/ThemedContent.jsx
@@ -1,6 +1,17 @@
 import { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 
-const ThemedContent = () => {}
+const ThemedContent = () => {
+  const { theme } = useContext(ThemeContext)
+
+  return (
+    <section className={`content ${theme}`}>
+      <h1>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</h1>
+      <p>
+        This is an example of content that changes based on the selected theme.
+      </p>
+    </section>
+  )
+}
 
 export default ThemedContent
