87f47ee20d01c6f455b239d3af3ac6d7ff451ae7
Espadv69 <aitorjob69@gmail.com>
2025-04-08 17:51:08 +0200

    feat: add imports for AuthStatus, LoginButton, LogoutButton, and AuthProvider in App component

--- AuthContextDemo-Context/src/App.jsx
+++ AuthContextDemo-Context/src/App.jsx
@@ -1,3 +1,8 @@
+import AuthStatus from './components/AuthStatus'
+import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton'
+import { AuthProvider } from './context/AuthContext'
+
 const App = () => {}
 
 export default App
