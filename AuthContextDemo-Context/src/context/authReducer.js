07da6e701d6fe95110c6e6bbf543ce4be6614816
Espadv69 <aitorjob69@gmail.com>
2025-04-08 12:02:49 +0200

    feat: implement authReducer for managing authentication state

---
+++ AuthContextDemo-Context/src/context/authReducer.js
@@ -0,0 +1,22 @@
+import { LOGIN, LOGOUT } from './authTypes'
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+      }
+
+    case LOGOUT:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      }
+
+    default:
+      return state
+  }
+}
