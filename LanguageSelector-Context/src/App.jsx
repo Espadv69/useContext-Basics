5f83a6db7175be5a67d41213ae5fe99dea9d83a6
Espadv69 <aitorjob69@gmail.com>
2025-04-09 19:43:34 +0200

    feat: implement App component to render LanguageSelector and Greeting

--- LanguageSelector-Context/src/App.jsx
+++ LanguageSelector-Context/src/App.jsx
@@ -1,6 +1,14 @@
 import LanguageSelector from './components/LanguageSelector'
 import Greeting from './components/Greeting'
 
-const App = () => {}
+const App = () => {
+  return (
+    <main>
+      <h1 className="title">🌐 Language Context Demo</h1>
+      <LanguageSelector />
+      <Greeting />
+    </main>
+  )
+}
 
 export default App
