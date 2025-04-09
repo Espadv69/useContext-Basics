d663cf20bd46b3514f484291dc3bcc66a05a3e72
Espadv69 <aitorjob69@gmail.com>
2025-04-09 19:33:19 +0200

    feat: add handleChange function to LanguageSelector for language change handling

--- LanguageSelector-Context/src/components/LanguageSelector.jsx
+++ LanguageSelector-Context/src/components/LanguageSelector.jsx
@@ -2,6 +2,10 @@ import { useLanguage } from '../context/LanguageContext'
 
 const LanguageSelector = () => {
   const { language, changeLanguage } = useLanguage()
+
+  const handleChange = (e) => {
+    changeLanguage(e.target.value)
+  }
 }
 
 export default LanguageSelector
