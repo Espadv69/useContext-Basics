f595d9388124bccf596da3f671d6e1b91e29601e
Espadv69 <aitorjob69@gmail.com>
2025-04-09 17:12:13 +0200

    feat: implement languageReducer for managing language state

--- LanguageSelector-Context/src/context/languageReducer.js
+++ LanguageSelector-Context/src/context/languageReducer.js
@@ -1,1 +1,14 @@
 import { SET_LANGUAGE } from './languageTypes'
+
+export const languageReducer = (state, action) => {
+  switch (action.type) {
+    case SET_LANGUAGE:
+      return {
+        ...state,
+        language: action.payload,
+      }
+
+    default:
+      return state
+  }
+}
