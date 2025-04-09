b2befe04fb4c9f5e49e13bf3d0d7306454ea598e
Espadv69 <aitorjob69@gmail.com>
2025-04-09 17:13:59 +0200

    feat: initialize LanguageContext and define initial state for language management

--- LanguageSelector-Context/src/context/LanguageContext.jsx
+++ LanguageSelector-Context/src/context/LanguageContext.jsx
@@ -1,3 +1,9 @@
 import { createContext, useContext, useReducer } from 'react'
 import { languageReducer } from './languageReducer'
 import { SET_LANGUAGE } from './languageTypes'
+
+const LanguageContext = createContext()
+
+const initialState = {
+  language: 'en',
+}
