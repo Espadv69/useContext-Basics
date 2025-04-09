2c18e96ab553eae155f08f2c31031dba5b5bb7ea
Espadv69 <aitorjob69@gmail.com>
2025-04-09 19:41:01 +0200

    feat: wrap App component with LanguageProvider for context support

--- LanguageSelector-Context/src/main.jsx
+++ LanguageSelector-Context/src/main.jsx
@@ -2,6 +2,7 @@ import '@fontsource-variable/roboto'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LanguageProvider } from './context/LanguageContext.jsx'
 
 import App from './App.jsx'
 import './index.css'
@@ -10,6 +11,8 @@ const root = createRoot(document.getElementById('root'))
 
 root.render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 )
