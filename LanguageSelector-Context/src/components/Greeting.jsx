ddebab78dafdcf5a1374554999e63d95c756866c
Espadv69 <aitorjob69@gmail.com>
2025-04-09 19:39:31 +0200

    feat: add GREETINGS object to Greeting component for multilingual support

--- LanguageSelector-Context/src/components/Greeting.jsx
+++ LanguageSelector-Context/src/components/Greeting.jsx
@@ -2,6 +2,12 @@ import { useLanguage } from '../context/LanguageContext'
 
 const Greeting = () => {
   const { language } = useLanguage()
+
+  const GREETINGS = {
+    en: 'Hello! How are you? 👋',
+    es: '¡Hola! ¿Cómo estás? 👋',
+    fr: 'Bonjour! Comment ça va? 👋',
+  }
 }
 
 export default Greeting
