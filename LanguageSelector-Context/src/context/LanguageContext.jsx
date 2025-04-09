9d562d67addfec4ba3f009dbf1fd10e2dda4c3c0
Espadv69 <aitorjob69@gmail.com>
2025-04-09 17:18:18 +0200

    feat: add useLanguage hook for accessing language context

--- LanguageSelector-Context/src/context/LanguageContext.jsx
+++ LanguageSelector-Context/src/context/LanguageContext.jsx
@@ -21,3 +21,12 @@ export const LanguageProvider = ({ children }) => {
     </LanguageContext.Provider>
   )
 }
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext)
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider')
+  }
+
+  return context
+}
