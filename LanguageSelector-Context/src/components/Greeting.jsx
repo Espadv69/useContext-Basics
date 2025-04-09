a669dc9af88cd290c3c32bcde56c0e772be3289e
Espadv69 <aitorjob69@gmail.com>
2025-04-09 19:51:00 +0200

    refactor: enhance Greeting component by organizing messages into separate constants for clarity

--- LanguageSelector-Context/src/components/Greeting.jsx
+++ LanguageSelector-Context/src/components/Greeting.jsx
@@ -9,10 +9,16 @@ const Greeting = () => {
     fr: 'Bonjour! Comment ça va? 👋',
   }
 
-  const MESSAGES = {
+  const MESSAGES_CURRENT = {
     en: `Current language: ${language}`,
     es: `Idioma actual: ${language}`,
     fr: `Langue actuelle: ${language}`,
+  }
+
+  const WHERE_MESSAGES = {
+    en: 'Change the language in the top right corner!',
+    es: '¡Cambia el idioma en la esquina superior derecha!',
+    fr: 'Changez la langue dans le coin supérieur droit!',
   }
 
   const CURRENT_LANGUAGE = GREETINGS[language]
@@ -21,10 +27,10 @@ const Greeting = () => {
   }
 
   return (
-    <section className="greeting">
-      <h1>{CURRENT_LANGUAGE}</h1>
-      <p>{MESSAGES[language]}</p>
-      <p>Change the language in the top right corner!</p>
+    <section className="greeting-section">
+      <h1 className="greeting">{CURRENT_LANGUAGE}</h1>
+      <p className="greeting_message">{MESSAGES_CURRENT[language]}</p>
+      <p className="greeting_message">{WHERE_MESSAGES[language]}</p>
     </section>
   )
 }
